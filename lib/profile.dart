17731626e78823e5b27c17b5fad43d6f0f98edd6
Mark Greenaway <markg@maths.usyd.edu.au>
2019-06-01 13:08:01 +1000

    Add imageURL to Profile class.

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,14 +5,16 @@ class Profile {
     String company;
     List<String> contactDetails;
     List<String> portfolio;
+    String imageURL;
 
-    Profile(name, skills, biography, company, contactDetails, portfolio) {
+    Profile(name, skills, biography, company, contactDetails, portfolio, imageURL) {
         this.name = name;
         this.skills = skills;
         this.biography = biography;
         this.company = company;
         this.contactDetails = contactDetails;
         this.portfolio = portfolio;
+        this.imageURL = imageURL;
     }
 
 
